f11f7857b5f74d4f0656d0b1270933a9c2d6a155
ManshuTyagi <himanshut567@gmail.com>
2026-02-20 20:02:02 +0100

    MT6893: Build libutils-v32

--- device.mk
+++ device.mk
@@ -495,6 +495,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcrypto-v32 \
     libssl-v32 \
+    libutils-v32 \
     libhidlbase-v32.vendor \
     libstagefright_foundation_v33 \
     libtinyalsa-v32
